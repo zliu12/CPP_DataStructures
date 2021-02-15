ea5253c0ab081a50087c893b10429a421bc1a012
QiChang Liu <qcl@QiChangdeAir.attlocal.net>
2021-02-15 03:58:24 -0800

    project/programming_project structure update to class

--- project/programming_project/programming_project.cpp
+++ project/programming_project/programming_project.cpp
@@ -12,145 +12,149 @@
 #include <cctype>
 #include <ctime>
 
+typedef std::vector<std::string> str_vec;
+
 const int YEAR_LOWER_BOUND = 1900;
 const int YEAR_UPPER_BOUND = 2021;
+const int ZODIAC_SIZE = 12;
 const int INPUT_LENGTH = 4;
 
-// Check if input characters are digits
-bool checkDigit(const std::string &s) {
-  for (unsigned i = 0; i < s.size(); ++i) {
-    if (!isdigit(s[i])) {
-      std::cout << "Invalid input: Non-numerical character exists.\n"
+class ChineseZodiac {
+ private:
+  str_vec _zodiac_list = {"Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox",
+    "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat"};
+
+ public:  
+  // Access private _zodiac_list
+  str_vec get_zodiac_list() const {
+    return _zodiac_list;
+  }
+
+  // Take input of year of birth
+  std::string get_year_input(std::string year) {
+    std::cout << "Welcome to Find Your Chinese Zodiac - The Twelve Animal "
+              << "Guardians\n" << std::endl;
+    std::cout << "Please enter your year of birth (YYYY): ";
+    std::getline(std::cin, year);
+    std::cout << std::endl;
+    return year;
+  }
+
+  // Check if input characters are digits
+  bool checkDigit(const std::string &s) const {
+    for (unsigned i = 0; i < s.size(); ++i) {
+      if (!isdigit(s[i])) {
+        std::cout << "Invalid input: Non-numerical character exists.\n"
+                  << std::endl;
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // Check if input is within the time frame
+  bool checkTime(const std::string &s) const {
+    if (0 <= std::stoi(s) && std::stoi(s) < YEAR_LOWER_BOUND) {
+      std::cout << "Invalid input: Out of time frame. The Man From Earth?\n"
+                << std::endl;
+      return false;
+    } else if (std::stoi(s) > YEAR_UPPER_BOUND) {
+      std::cout << "Invalid input: Out of time frame. Back To The Future!\n"
                 << std::endl;
       return false;
     }
-  }
-  return true;
-}
-
-// Check input size
-bool checkLength(const std::string &s) {
-  if (s.size() != INPUT_LENGTH) {
-    std::cout << "Invalid input: Wrong number of characters entered.\n"
-              << std::endl;
-    return false;
-  }
-  return true;
-}
-
-// Check input time frame
-bool checkTimeFrame(const std::string &s) {
-  if (0 <= std::stoi(s) && std::stoi(s) < YEAR_LOWER_BOUND) {
-    std::cout << "Invalid input: Out of time frame. The Man From Earth?\n"
-              << std::endl;
-    return false;
-  } else if (std::stoi(s) > YEAR_UPPER_BOUND) {
-    std::cout << "Invalid input: Out of time frame. Back To The Future!\n"
-              << std::endl;
-    return false;
-  }
-  return true;
-}
-
-// Validate input
-bool validate_year_of_birth(const std::string &s) {
-  if (checkDigit(s) && checkLength(s) && checkTimeFrame(s)) {
     return true;
   }
-  return false;
-}
 
-// Input year of birth
-std::string input_year_of_birth() {
-  std::string year_str;
-  std::cout << "Welcome to Find Your Chinese Zodiac - The Twelve Animal "
-            << "Guardians" << std::endl << std::endl;
-  std::cout << "Please enter your year of birth (YYYY): ";
-  std::getline(std::cin, year_str);
-  std::cout << std::endl;
-  return year_str;
-}
-
-// Find your animal guardian
-void animal_guardian(const std::string &s, const std::vector<std::string> &v) {
-  int n = std::stoi(s);
-  int result = n % v.size();
-  switch (result) {
-    case 0:
-      std::cout << "Your animal guardian is: "  << v[result] << std::endl;
-      break;
-    case 1:
-      std::cout << "Your animal guardian is: "  << v[result] << std::endl;
-      break;
-    case 2:
-      std::cout << "Your animal guardian is: "  << v[result] << std::endl;
-      break;
-    case 3:
-      std::cout << "Your animal guardian is: "  << v[result] << std::endl;
-      break;
-    case 4:
-      std::cout << "Your animal guardian is: "  << v[result] << std::endl;
-      break;
-    case 5:
-      std::cout << "Your animal guardian is: "  << v[result] << std::endl;
-      break;
-    case 6:
-      std::cout << "Your animal guardian is: "  << v[result] << std::endl;
-      break;
-    case 7:
-      std::cout << "Your animal guardian is: "  << v[result] << std::endl;
-      break;
-    case 8:
-      std::cout << "Your animal guardian is: "  << v[result] << std::endl;
-      break;
-    case 9: 
-      std::cout << "Your animal guardian is: "  << v[result] << std::endl;
-      break;     
-    case 10: 
-      std::cout << "Your animal guardian is: "  << v[result] << std::endl;
-      break;
-    case 11:
-      std::cout << "Your animal guardian is: "  << v[result] << std::endl;
-      break;
-  }
-}
-
-// find the next guardian year
-void next_guardian_year (const std::string &s) {
-  int n = stoi(s);
-  time_t t = time(NULL);
-  tm *tptr = localtime(&t);
-  while (n < tptr->tm_year + 1900) {
-    n += 12;
+  // Validate input
+  bool validate_year_of_birth(const std::string &s) const {
+    if (checkDigit(s) && checkTime(s)) {
+      return true;
+    }
+    return false;
   }
 
-  if (n == (tptr->tm_year + 1900)) {
-    std::cout << "This year is your guardian year!\n";
-    std::cout << "The year of " << n + 12 
-              << " will be your next guardian year." << std::endl;
-  } else {
-    std::cout << "The year of " << n << " will be your next guardian year."
+  // Find your animal guardian
+  void animal_guardian(const std::string &s, const str_vec &v) const {
+    int n = std::stoi(s);
+    int result = n % v.size();
+    switch (result) {
+      case 0:
+        std::cout << "Your animal guardian is: "  << v[result] << std::endl;
+        break;
+      case 1:
+        std::cout << "Your animal guardian is: "  << v[result] << std::endl;
+        break;
+      case 2:
+        std::cout << "Your animal guardian is: "  << v[result] << std::endl;
+        break;
+      case 3:
+        std::cout << "Your animal guardian is: "  << v[result] << std::endl;
+        break;
+      case 4:
+        std::cout << "Your animal guardian is: "  << v[result] << std::endl;
+        break;
+      case 5:
+        std::cout << "Your animal guardian is: "  << v[result] << std::endl;
+        break;
+      case 6:
+        std::cout << "Your animal guardian is: "  << v[result] << std::endl;
+        break;
+      case 7:
+        std::cout << "Your animal guardian is: "  << v[result] << std::endl;
+        break;
+      case 8:
+        std::cout << "Your animal guardian is: "  << v[result] << std::endl;
+        break;
+      case 9: 
+        std::cout << "Your animal guardian is: "  << v[result] << std::endl;
+        break;     
+      case 10: 
+        std::cout << "Your animal guardian is: "  << v[result] << std::endl;
+        break;
+      case 11:
+        std::cout << "Your animal guardian is: "  << v[result] << std::endl;
+        break;
+    }
+  }
+
+  // find the next guardian year
+  void next_guardian_year (const std::string &s) const {
+    int n = stoi(s);
+    time_t t = time(NULL);
+    tm *tptr = localtime(&t);
+    while (n < tptr->tm_year + 1900) {
+      n += 12;
+    }
+
+    if (n == (tptr->tm_year + 1900)) {
+      std::cout << "This year is your guardian year!\n";
+      std::cout << "The year of " << n + 12 
+                << " will be your next guardian year." << std::endl;
+    } else {
+      std::cout << "The year of " << n << " will be your next guardian year."
+                << std::endl;
+    }
+    std::cout << "Remember wearing a red will bring you good luck!" 
               << std::endl;
   }
-  std::cout << "Remember wearing a red will bring you good luck!" << std::endl;
-}
+};
 
 int main() {
-  // Create a vector to hold 12 zodiac
-  std::vector<std::string> zodiac_vec = {"Monkey", "Rooster", "Dog", "Pig", "Rat", 
-    "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat"};
+  // Create an object
+  ChineseZodiac zodiac_user; 
+  std::string year_input;
   
   // Take input and validate
-  std::string year_of_birth_str;
   do {
-    year_of_birth_str = input_year_of_birth();
-  } while (!validate_year_of_birth(year_of_birth_str));
+    year_input = zodiac_user.get_year_input(year_input);
+  } while (!zodiac_user.validate_year_of_birth(year_input));
 
   // Output animal guardian result
-  animal_guardian(year_of_birth_str, zodiac_vec);
+  zodiac_user.animal_guardian(year_input, zodiac_user.get_zodiac_list());
 
   // Output the next animal guardian year
-  next_guardian_year(year_of_birth_str);
+  zodiac_user.next_guardian_year(year_input);
 
   return 0;
 }
